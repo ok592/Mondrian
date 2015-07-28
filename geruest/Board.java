d14510ba3035b158b93b5ae7d78f238c5f546ab2
ok592 <a.n.y@live.at>
2015-07-28 20:18:09 +0200

    Update Board.java

--- geruest/Board.java
+++ geruest/Board.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Board extends JPanel implements ActionListener {
@@ -13,8 +14,27 @@ public class Board extends JPanel implements ActionListener {
     Polys polys;
     private Timer timer;
     private Player player;
+    private Enemy enemy;
     private CopyOnWriteArrayList<Point2D> currentPointList = new CopyOnWriteArrayList<>();
+    private ArrayList<Point> borderPointList = new ArrayList<>();
+    private ArrayList<Point> whereWasI = new ArrayList<>();
     private boolean[][] allPoints = new boolean[200][200];
+    private int percentage = 0;
+    private int offset;
+    private int enemyOffset;
+
+    private final Color[] COLORS = new Color[]{Color.red, Color.yellow, Color.blue, Color.black, Color.lightGray};
+    //array of colors a polygon can be drawn in.
+
+    private final String[] WINMSGS = new String[]{"You actually did it, you absolute madman!", "I stand in awe!", "Awesome, you won",
+            "You made the right choices.", "You only got lucky, next time you wont´t get away that easyly.", "WINRAR!"};
+    //array of messages. one of those is shown if the game was lost.
+
+    private final String[] LOSTMSGS = new String[]{"C´mon man, u can do better!", "Das war suboptimal..", "Spielst du um zu gewinnen, oder um diese Nachricht zu sehen?",
+            "You´re embarrassing yourself!", "That´s not how you play this game", "Your score is 0.", "Vielleicht beim nächsten Mal, aber ich glaube eher nicht", "Dein wievielter " +
+            "Versuch war das jetzt? Ich hab aufgehört mitzuzählen", "You lost. Again.", "Here I stand again, THE LOSER!"};
+    //array of messages. one of those is shown if the game was lost.
+
 
     public Board() {
         initBoard();
@@ -25,15 +45,27 @@ public class Board extends JPanel implements ActionListener {
         setFocusable(true);
         setBackground(Color.white);
 
+        setPreferredSize(new Dimension(200, 200));
+        setDoubleBuffered(true);
+
+
         player = new Player();
+        enemy = new Enemy();
+
+        offset = player.getHeight()/2;
+        enemyOffset = enemy.getHeight()/2;
 
         polys = new Polys();
 
         for (int i = 0; i < 200; i++) {
             allPoints[0][i] = true;
+            borderPointList.add(new Point(0, i));
             allPoints[199][i] = true;
+            borderPointList.add(new Point(199, i));
             allPoints[i][0] = true;
+            borderPointList.add(new Point(i, 0));
             allPoints[i][199] = true;
+            borderPointList.add(new Point(i, 199));
         }
 
         timer = new Timer(DELAY, this);
@@ -43,7 +75,27 @@ public class Board extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         player.move();
+
+        int liveAndLetLive = (int)(Math.random()*100000);
+        if(liveAndLetLive < 1000){
+            enemy.kill(new Point(player.getX(), player.getY()));
+        }
+        enemy.move(allPoints);
+
+
         repaint();
+
+        //checks if >80% have been filled, and shows a popup
+        if(didIWinYet()){
+            JOptionPane.showMessageDialog(null, WINMSGS[(int)(Math.random()*WINMSGS.length)] + "\n" + "Your Score is "
+                    + String.format("%,d", percentage) + "!", "You Win!", 3);
+            System.exit(0);
+        }
+        //checks if there was a collission between a line which is currently being drawn or the player and the enemy.
+        if(hitBox()){
+            JOptionPane.showMessageDialog(null, LOSTMSGS[(int)(Math.random()*LOSTMSGS.length)], "You Lost", 3);
+            System.exit(0);
+        }
     }
 
     @Override
@@ -70,19 +122,25 @@ public class Board extends JPanel implements ActionListener {
         if (x >= 0 && x < 200 && y >= 0 && y < 200) {
             //allPoints[x][y] = true;
             currentPointList.add(new Point(x, y));
+            whereWasI.add(new Point(x,y));
         }
 
         g2d.setStroke(new BasicStroke(2));
 
-        g2d.setColor(Color.blue);
+        g2d.setColor(Color.black);
+        drawLine(g2d);
+
+        //COLORS[(int) (Math.random() * COLORS.length)]
+        g2d.setColor(Color.yellow);
         //polys.drawPolys(g2d);
         drawPoints(g2d);
 
-        g2d.setColor(Color.red);
-        drawLine(g2d);
-
-        g2d.setColor(Color.green);
+        g2d.setColor(Color.blue);
         g2d.fillOval(x, y, player.getWidth(), player.getHeight());
+
+        g2d.setColor(Color.red);
+        g2d.fillOval(enemy.getX(), enemy.getY(), enemy.getWidth(), enemy.getHeight());
+
 
     }
 
@@ -91,6 +149,7 @@ public class Board extends JPanel implements ActionListener {
             for (int j = 0; j < allPoints[i].length; j++) {
                 if (allPoints[i][j]) {
                     g2d.drawLine(i, j, i, j);
+                    g2d.drawLine(i+offset, j+offset, i+offset, j+offset);
                 }
             }
         }
@@ -99,8 +158,9 @@ public class Board extends JPanel implements ActionListener {
     private void drawLine(Graphics g) { //draw a line out of the current points
         Graphics2D g2d = (Graphics2D) g;
 
-        for (Point2D c : currentPointList) {
-            g2d.drawLine((int) c.getX(), (int) c.getY(), (int) c.getX(), (int) c.getY());
+        for (Point c : whereWasI) {
+            g2d.drawLine((int) c.getX()+offset, (int) c.getY()+offset, (int) c.getX()+offset, (int) c.getY()+offset); // offset so that line
+            //is centered
         }
     }
 
@@ -121,6 +181,84 @@ public class Board extends JPanel implements ActionListener {
             allPoints[((int) aCurrentPointList.getY())][((int) aCurrentPointList.getY())] = true;
         }
     }
+
+    private boolean didIWinYet(){
+        percentage = 0;
+        for(boolean[] b : allPoints){
+            for(boolean c : b){
+                if(c){
+                    percentage++;
+                }
+            }
+        }
+
+        return ((percentage*100)/(200*200) >= 80);
+    }
+
+    //calculates a hit between either the player and the enemy or a line currently being drawn and the enemy
+    //if either one is a hit, the game is lost and a message pops up (see keypressed method)
+    //may not be the best way to do this, but works.
+    private boolean hitBox(){
+
+        int a = player.getX();
+        int b = player.getY();
+
+        boolean xHit = false;
+        boolean yHit = false;
+
+        boolean lineHit;
+        int lineX = -99;
+        int lineY = -99;
+
+        //checks for the whole size of the player and the enemy in x and y coordinates. if there is a hit in x AND
+        //y coordinates, the player has touched the enemy and thus lost the game.
+        for(int i = player.getX(); i<=player.getX()+player.getHeight(); i++){
+            for(int j = enemy.getX(); j<=enemy.getX()+enemy.getHeight(); j++){
+                if(i==j) {
+                    xHit = true;
+                    break;
+                }
+            }
+        }
+        for(int i = player.getY(); i<=player.getY()+player.getHeight(); i++){
+            for(int j = enemy.getY(); j<=enemy.getY()+enemy.getHeight(); j++) {
+                if (i == j) {
+                    yHit = true;
+                    break;
+                }
+            }
+        }
+
+        //checks every point in the line currently being drawn with the position of the enemy.
+        //if both exist, the enemy has touched a line currently being drawn which means the player has lost the game.
+        for(Point2D p : currentPointList){
+            for(int k = enemy.getX(); k<=enemy.getX()+enemy.getHeight(); k++){
+                if(p.getX()==k) {
+                    lineX = (int)p.getX();
+                    break;
+                }
+            }
+        }
+
+        for(Point2D p : currentPointList){
+            for(int l = enemy.getY(); l<=enemy.getY()+enemy.getHeight(); l++){
+                if(p.getY()==l) {
+                    lineY = (int)p.getY();
+                    break;
+                }
+            }
+        }
+
+        //just because there is a matching x and y variable somewhere in the points of the current line doesn´t mean
+        //the enemy has actually touched the line. those x and y variables must come from the same point.
+        Point hitpoint = new Point(lineX, lineY);
+
+        //and the deque must contain this point.
+        lineHit = currentPointList.contains(hitpoint) && !borderPointList.contains(hitpoint);
+
+        return ((xHit&&yHit) || lineHit) && !allPoints[a][b];
+    }
+
 
     private class TAdapter extends KeyAdapter {
 
